c96d8a2b15a4bb7eadd2eeeb1b135c6f68aa7e4d
Sharvari Deshpande <39101080+sharvaridesh@users.noreply.github.com>
2019-02-28 20:10:23 -0500

    Update add binary numbers.py

--- add binary numbers.py
+++ add binary numbers.py
@@ -14,19 +14,3 @@ class Solution(object):
         """
         sum1 = (int(a,2)+int(b,2))
         return bin(sum1).replace("0b","")
-    
-    
-    
-
-#a="11"
-#b="100"
-#sum1 = (int(a,2)+int(b,2))
-#print(sum1)
-#binary1 = sum1
-#decimal, i, n = 0, 0, 0
-#while(sum1 != 0): 
-#    dec = sum1 % 10
-#    decimal = decimal + dec * pow(2, i) 
-#    sum1 = sum1//10
-#    i += 1
-#print(decimal)
